7dc8595d52a87388df26a5972166ad19b14855f2
ItsJustLukas78 <somwonglukas@gmail.com>
2023-05-21 19:02:53 -0700

    parsing handling

--- langchain/agents/conversational_chat/output_parser.py
+++ langchain/agents/conversational_chat/output_parser.py
@@ -26,7 +26,23 @@ class ConvoOutputParser(AgentOutputParser):
             if cleaned_output.endswith("```"):
                 cleaned_output = cleaned_output[: -len("```")]
             cleaned_output = cleaned_output.strip()
-            response = json.loads(cleaned_output)
+
+            # response = json.loads(cleaned_output)
+
+            try:
+                response = json.loads(cleaned_output)
+            except json.JSONDecodeError:
+                response = {}
+                cleaned_output = cleaned_output.replace('\n', '')
+
+                try:
+                    response["action"] = cleaned_output.split('"action": "', 1)[1].split('",', 1)[0].strip()
+                    response["action_input"] = cleaned_output.split('"action_input": "', 1)[1].split('"}', 1)[0].strip()
+                except IndexError:
+
+                    raise ValueError(
+                        "Invalid input format. Unable to extract 'action' and 'action_input' from the text.")
+
             action, action_input = response["action"], response["action_input"]
             if action == "Final Answer":
                 return AgentFinish({"output": action_input}, text)
